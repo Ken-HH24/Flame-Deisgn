38c0c1c09f96231adba2763a29a64a9b5ab1bc9f
Ken-HH24 <1098862703@qq.com>
2021-09-30 16:26:58 +0800

    feat: alert

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,28 @@
 import React from 'react';
-import Button, { ButtonSize, ButtonType } from './components/Button/button';
+import Alert, { AlertType } from './components/Alert/alert';
 
 function App() {
   return (
     <div className="App">
-      <Button
-        size={ButtonSize.Large}
-        disabled={true}
-      >Hello</Button>
+      <Alert
+        title='aaa'
+        content='bbbbb'
+        closeable={false}
+      />
+      <Alert
+        content='bbbbb'
+        type={AlertType.Danger}
+      />
 
-      <Button
-        btnType={ButtonType.Link}
-        href='http://baidu.com'
-      >Hello</Button>
-      <Button
-        btnType={ButtonType.Link}
-        href='http://baidu.com'
-        disabled={true}
-      >Hello</Button>
-
-      <Button
-        size={ButtonSize.Large}
-        btnType={ButtonType.Primary}
-      >Hello</Button>
-
-      <Button
-        size={ButtonSize.Large}
-        btnType={ButtonType.Danger}
-      >Hello</Button>
-      <Button
-        size={ButtonSize.Large}
-        btnType={ButtonType.Success}
-      >Hello</Button>
-      <Button
-        size={ButtonSize.Large}
-        btnType={ButtonType.Warning}
-      >Hello</Button>
+      <Alert
+        title='aaa'
+        content='bbbbb'
+        type={AlertType.Warning}
+      />
+      <Alert
+        content='bbbbb'
+        type={AlertType.Success}
+      />
     </div>
   );
 }
---
+++ src/components/Alert/alert.tsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import classNames from '../../utils/classNames';
+
+export enum AlertType {
+    Default = 'default',
+    Success = 'success',
+    Warning = 'warning',
+    Danger = 'danger'
+}
+
+interface BaseAlertProps{
+    className?: string;
+    type?: AlertType;
+    title?: string;
+    content?: string;
+    closeable?: boolean;
+}
+
+const Alert: React.FC<BaseAlertProps> = (props) => {
+    const {
+        className,
+        type,
+        title,
+        content,
+        closeable,
+        ...restProps
+    } = props;
+
+    const classes = classNames(
+        'alert',
+        className,
+        {
+            [`alert-${type}`]: type
+        }
+    );
+    const [isShow, setIsShow] = useState(true);
+
+    return (
+        <>
+            {
+                isShow &&
+                (
+                    <div
+                        className={classes}
+                        {...restProps}
+                    >
+                        {title && <h4 className='alert-title'>{title}</h4>}
+                        {closeable && <i className='closed-icon' onClick={()=>{setIsShow(false)}}></i>}
+                        <span className='alert-content'>{content}</span>
+                    </div>
+                )
+            }
+        </>
+    );
+}
+
+Alert.defaultProps = {
+    type: AlertType.Default,
+    closeable: true
+}
+
+export default Alert;
